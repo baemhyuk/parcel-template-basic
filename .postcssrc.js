7020dff1073b1131999aa1a9ee8dd7020dc3db87
minhyuk <minhyuk@minhyukui-MacBookPro.local>
2021-05-31 15:46:04 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+
+//import autoprefixer from 'autoprefixer'
+
+// export {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+
+// CommonJs
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  plugins: [autoprefixer],
+};
